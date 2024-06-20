7b5d3416fd9f3f30a71a40ba3cdb8b80b1e40cc8
rakechen-0307 <rakechen168@gmail.com>
2024-06-20 16:39:28 +0800

    add evaluate script

---
+++ scripts/evaluate_coco.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python train.py --epochs 100 --optimizer Adam --lr 0.001 --compress policies/schedule.yaml --model obstacle_detection --use-bias --momentum 0.9 --weight-decay 5e-4 --dataset coco --data ./data/coco --device MAX78000 --obj-detection --obj-detection-params parameters/obj_detection_params.yaml --qat-policy policies/qat_policy.yaml --batch-size 16 --evaluate -8 --exp-load-weights-from ../ai8x-synthesis/trained/obstacle_detection-qat8-q.pth.tar --save-sample 10 "$@" 
